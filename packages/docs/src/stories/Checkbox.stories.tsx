e4aac3b8f85c16921748bcb023aa9a10fb2fab9e
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-12-29 15:46:34 -0300

    feat: add checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,23 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+
+const stories: Meta<CheckboxProps> = {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', alignItems: 'center', gap: '$2' }}
+      >
+        {Story()}
+        <Text size="sm">Accept terms and conditions</Text>
+      </Box>
+    ),
+  ],
+};
+
+export default stories;
+
+export const Primary: StoryObj<CheckboxProps> = {};

5d4bdf650d0e36eaca0ab038b8ec47de5a78e3be
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-12-28 22:27:25 -0300

    feat: storybook setup with vite

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,29 @@
-import { colors } from '@ignite-ui/tokens';
+import { ComponentProps } from 'react';
 
 import { styled } from './styles';
 
-const Button = styled('button', {
-  fontFamily: '$default',
+export const Button = styled('button', {
   backgroundColor: '$ignite300',
-  height: '$10',
+  border: 0,
+  borderRadius: '$sm',
+  color: '$white',
+  fontFamily: '$default',
+  fontWeight: 'bold',
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
 });
 
-export function App() {
-  return <Button style={{ color: colors.white }}>Hello World</Button>;
-}
+export type ButtonProps = ComponentProps<typeof Button>;
